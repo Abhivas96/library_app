f15bbd8a62f04b91a53187f46ea05e951c0f96e0
Abhishek Kumar <110897857+Abhivas96@users.noreply.github.com>
2024-01-17 21:00:02 +0530

    Add files via upload

---
+++ library_app/library_backend/src/main/java/com/Abhishek/library_backend/dao/ReviewRepository.java
@@ -0,0 +1,25 @@
+package com.Abhishek.library_backend.dao;
+
+import com.Abhishek.library_backend.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Override
+    void deleteAllById(Iterable<? extends Long> longs);
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+
+    Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
+
+    @Modifying
+    @Query("delete from Review where bookId in :bookId")
+    void deleteAllByBookId(@Param("bookId") Long bookId);
+}
---
+++ library_app/library_backend/src/main/java/com/Abhishek/library_backend/requestModels/AdminQuestionRequest.java
@@ -0,0 +1,10 @@
+package com.Abhishek.library_backend.requestModels;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+    private String response;
+}
